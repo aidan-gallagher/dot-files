007deaaec60ef009c16f3ed944a61fbc3c69118f
aidan-gallagher <67737561+aidan-gallagher@users.noreply.github.com>
2022-03-16 14:34:53 +0000

    Create vscode-extensions.sh

---
+++ vscode-extensions.sh
@@ -0,0 +1,5 @@
+# code --list-extensions | xargs -L 1 echo code --install-extension
+
+code --install-extension ue.alphabetical-sorter
+code --install-extension stackbreak.comment-divider
+code --install-extension eamodio.gitlens
