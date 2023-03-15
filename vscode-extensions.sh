77451980eb848d86b7adfd61677241d928e98a02
Aidan Gallagher <67737561+aidan-gallagher@users.noreply.github.com>
2023-03-15 10:34:03 +0000

    Update vscode-extensions.sh

--- vscode-extensions.sh
+++ vscode-extensions.sh
@@ -1,5 +1,7 @@
+# To generate this list from currently installed extensions
 # code --list-extensions | xargs -L 1 echo code --install-extension
 
+# Copy and paste these commands into a terminal to install
 code --install-extension ue.alphabetical-sorter
 code --install-extension stackbreak.comment-divider
 code --install-extension eamodio.gitlens
